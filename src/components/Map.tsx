c67fe9c5dce0c4b65fd5759290b7bc514d292bf8
Joli Holmes <holmesjoli@gmail.com>
2023-12-19 11:25:14 -0800

    modualrized code

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,11 @@
-import { style } from '../utils/Global';
+// Libraries
+import { MapContainer, TileLayer, GeoJSON, ZoomControl, useMap } from "react-leaflet";
+
+// Data
+import states from "../data/states.json";
+import geoData from "../data/geoData.json";
+
+import { style} from "../utils/Global";
 
 export function mouseOver(event: any) {
     var layer = event.target;
@@ -11,4 +18,61 @@ export function mouseOver(event: any) {
 export function mouseOut(event: any) {
     var layer = event.target;
     layer.setStyle(style);
+}
+
+const features = [] as GeoJSON.Feature[];
+
+states.forEach((d: any) => {
+    features.push({type: 'Feature', 
+                   properties: {name: d.name, stfp: d.stfp, centroid: {lat: d.Y, long: d.X}}, 
+                   geometry: d.geometry})
+});
+
+const featuresCollection = {type: 'FeatureCollection', features: features} as GeoJSON.FeatureCollection;
+
+
+export default function Map({ setFullScreen, setState, setBounds }: { setFullScreen: any, setState: any, setBounds: any }): JSX.Element {
+
+    function onEachFeature(_: any, layer: any) {
+        layer.on({
+          mouseover: mouseOver,
+          mouseout: mouseOut,
+          click: onClick
+        });
+    }
+
+    function onClick(event: any) {
+        var layer = event.target;
+        setFullScreen(false);
+        setState(geoData.find(d => d.stfp === layer.feature.properties.stfp) as any);
+
+        const innerBounds = event.target.getBounds();
+        setBounds(innerBounds);
+
+        // console.log(bounds)
+        // map.fitBounds(innerBounds)
+    }
+
+    // function MyComponent() {
+    //     const map = useMap()
+    //     console.log('map center:', map.getCenter())
+    //     return null
+    // }
+
+    return(
+        <MapContainer
+            className="home-map"
+            center={[39.97, -86.19]}
+            zoom={5}
+            maxZoom={18}
+            scrollWheelZoom={false}
+            >
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors"
+            />
+            <GeoJSON data={featuresCollection} style={style} onEachFeature={onEachFeature}/>
+            <ZoomControl position="bottomright" />
+        </MapContainer>
+    );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,16 @@
 // Libraries
 import { useRef, useState, useMemo } from "react";
-import { MapContainer, TileLayer, GeoJSON, ZoomControl, useMap } from "react-leaflet";
 
 // Components
 import Main from '../components/Main';
 import { StateStatus, USStatus } from '../components/Status';
 import { QueryMenu } from "../components/Query";
-import { mouseOver, mouseOut } from "../components/Map";
+import Map from "../components/Map";
 
 // Data 
-import { style, changeYearData, equityIndicatorData } from "../utils/Global";
-import states from "../data/states.json";
-import geoData from "../data/geoData.json";
+import { changeYearData, equityIndicatorData } from "../utils/Global";
 
 // Setup data for GeoJSON
-const features = [] as GeoJSON.Feature[];
-
-states.forEach((d: any) => {
-    features.push({type: 'Feature', 
-                   properties: {name: d.name, stfp: d.stfp, centroid: {lat: d.Y, long: d.X}}, 
-                   geometry: d.geometry})
-});
-
-const featuresCollection = {type: 'FeatureCollection', features: features} as GeoJSON.FeatureCollection;
 
 const outerBounds = [
     [5.499550, -167.276413], //Southwest
@@ -38,19 +26,7 @@ export default function Home({}): JSX.Element {
     const [isFullScreen, setFullScreen] = useState(true);
     const [bounds, setBounds] = useState(outerBounds);
 
-    console.log(bounds);
-
-    // const map = useMap();
-
-    // const innerHandlers = useMemo(
-    //     () => ({
-    //       click() {
-    //         setBounds(innerBounds)
-    //         map.fitBounds(innerBounds)
-    //       },
-    //     }),
-    //     [map],
-    //   )
+    // console.log(bounds);
 
     const handleFullScreen = () => {
         // if (isFullScreen) {
@@ -59,50 +35,11 @@ export default function Home({}): JSX.Element {
         setFullScreen(!isFullScreen);
     };
 
-    function onEachFeature(_: any, layer: any) {
-        layer.on({
-          mouseover: mouseOver,
-          mouseout: mouseOut,
-          click: onClick
-        });
-    }
-
-    function onClick(event: any) {
-        var layer = event.target;
-        setFullScreen(false);
-        setState(geoData.find(d => d.stfp === layer.feature.properties.stfp) as any);
-
-        const innerBounds = event.target.getBounds();
-        setBounds(innerBounds);
-
-        console.log(bounds)
-        // map.fitBounds(innerBounds)
-    }
-
-    function MyComponent() {
-        const map = useMap()
-        console.log('map center:', map.getCenter())
-        return null
-    }
-
     return(
         <Main>
             {isFullScreen? <USStatus /> : <StateStatus equityIndicator={equityIndicator} setEquityIndicator={setEquityIndicator} />}
             <QueryMenu isFullScreen={isFullScreen} changeYear={changeYear} setChangeYear={setChangeYear} state={state} setState={setState} county={county} setCounty={setCounty}/>
-            <MapContainer
-                className="home-map"
-                center={[39.97, -86.19]}
-                zoom={5}
-                maxZoom={18}
-                scrollWheelZoom={false}
-                >
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution="&copy; <a href=&quot;https://www.openstreetmap.org/copyright&quot;>OpenStreetMap</a> contributors"
-                />
-                <GeoJSON data={featuresCollection} style={style} onEachFeature={onEachFeature}/>
-                <ZoomControl position="bottomright" />
-            </MapContainer>
+            <Map setFullScreen={setFullScreen} setState={setState} setBounds={setBounds}/>
     </Main>
     )
 }
