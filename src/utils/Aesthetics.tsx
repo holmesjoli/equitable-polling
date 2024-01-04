6256bd2f23e41a3270814f75f189b8ab8b09d4e7
Joli Holmes <holmesjoli@gmail.com>
2024-01-03 16:48:56 -0800

    organized colors and other things into theme

--- src/utils/Aesthetics.tsx
+++ src/utils/Aesthetics.tsx
@@ -1,11 +1,16 @@
 import * as d3 from 'd3';
 
-export const grey = {primary: '#757575', secondary: '#C6C6C6'};
+const grey = {primary: '#757575', secondary: '#C6C6C6'};
 const backgroundFill = '#FAF6F0';
 const focusColor = '#047391';
 
-export const layersStyle = {default: { color: grey.primary, fillColor: backgroundFill, fillOpacity: 0.5, weight: 1, pointer: 'cursor' },
-                            selected: { color: grey.primary, fillColor: backgroundFill, fillOpacity: 0, weight: 2 },
+export const theme = {
+    fontSize: 13,
+    grey: grey
+}
+
+export const layersStyle = {default: { color: theme.grey.primary, fillColor: backgroundFill, fillOpacity: 0.5, weight: 1, pointer: 'cursor' },
+                            selected: { color: theme.grey.primary, fillColor: backgroundFill, fillOpacity: 0, weight: 2 },
                             highlight: { color: focusColor, fillColor: focusColor},
                             greyOut: { color: '#EAEAEA', fillOpacity: 0.75, weight: 0 }
 }
@@ -13,18 +18,18 @@ export const layersStyle = {default: { color: grey.primary, fillColor: backgroun
 // Scales
 export const strokeColorScale = d3.scaleOrdinal()
   .domain(["increase", "nochange", "decrease"] )
-  .range(["#610063", grey.primary, "#E45729"] );
+  .range(["#610063", theme.grey.primary, "#E45729"] );
 
 export const fillColorScale = d3.scaleOrdinal()
   .domain(['-3', '-2', '-1', '0', '1', '2', '3'] )
-  .range(["#E45729", "#F28559", "#FBB18A", grey.secondary, "#C498A6", "#935485", "#610063"] );
+  .range(["#E45729", "#F28559", "#FBB18A", theme.grey.secondary, "#C498A6", "#935485", "#610063"] );
 
 export const rScale = d3.scaleSqrt()
   .domain([1, 30])
   .range([3, 15]);
 
 function getColor(d: any) {
-    return d ? backgroundFill : grey.primary;
+    return d ? backgroundFill : theme.grey.primary;
 }
 
 function getWeight(d: any) {
@@ -33,7 +38,7 @@ function getWeight(d: any) {
 
 export function highlightSelectedStyle(feature: any) {
     return {
-        color: grey.primary,
+        color: theme.grey.primary,
         fillColor: getColor(feature.properties!.selected),
         weight: getWeight(feature.properties!.selected),
         opacity: 1,
