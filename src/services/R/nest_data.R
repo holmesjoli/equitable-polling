e1d49f6dba3079ab223c5ec9ba59e4c8a34ee057
Joli Holmes <holmesjoli@gmail.com>
2024-01-19 09:11:37 -0800

    added processing steps for longitudinal data

--- src/services/R/nest_data.R
+++ src/services/R/nest_data.R
@@ -7,21 +7,21 @@ library(magrittr)
 #' Computes the bbox of the geometry that exists in the dataframe
 #' Returns dataframe
 getBbox <- function(df) {
-  
+
   bbox <- lapply(1:nrow(df), function(x) {
     bb <- df %>% 
       slice(x) %>% 
       sf::st_bbox()
-    
+
     df <- data.frame(xmin = bb[1],
                      xmax = bb[3],
                      ymin = bb[2],
                      ymax = bb[4])
     return(df)
   }) %>% bind_rows()
-  
+
   row.names(bbox) <- NULL
-  
+
   return(bbox)
 }
 
@@ -33,15 +33,15 @@ getCentroid <- function(df) {
     bind_cols(df %>% 
                 sf::st_centroid() %>% 
                 sf::st_coordinates()) 
-  
+
   return(df)
 }
 
 #' Get states
 #' Writes out a json file at the state level
-getStates <- function(stfp) {
+getStates <- function(state_fips, pth) {
   df <- tigris::states(cb = T) %>% 
-    filter(STATEFP %in% stfp) %>% 
+    filter(STATEFP %in% state_fips) %>% 
     select(NAME, STATEFP, GEOID, geometry) %>% 
     rename(stfp = STATEFP,
            name = NAME,
@@ -52,15 +52,15 @@ getStates <- function(stfp) {
   df <- getCentroid(df)
   
   exportJSON <- toJSON(df)
-  write(exportJSON, "../data/processed/stateGeoJSON.json")
+  write(exportJSON, file.path(pth, "stateGeoJSON.json"))
 }
 
 #' Get Counties
 #' Writes out a json file at the county
-getCounties <- function(stfp) {
+getCounties <- function(state_fips, pth) {
 
   df <- tigris::counties(cb = T) %>% 
-    filter(STATEFP %in% stfp) %>% 
+    filter(STATEFP %in% state_fips) %>% 
     select(STATEFP, NAME, COUNTYFP, GEOID, geometry) %>% 
     rename(stfp = STATEFP,
            cntyfp = COUNTYFP,
@@ -68,19 +68,18 @@ getCounties <- function(stfp) {
            geoid = GEOID)
   
   df <- cbind(df, getBbox(df))
-  df <- getCentroid(df) %>% 
-    arrange(stfp, name)
-  
+  df <- getCentroid(df)
+
   exportJSON <- toJSON(df)
-  write(exportJSON, "../data/processed/countyGeoJSON.json")
+  write(exportJSON, file.path(pth, "countyGeoJSON.json"))
 }
 
 #' Get Census tracts
 #' Writes out a json file at the census tract level
-getTracts <- function(stfp) {
+getTracts <- function(state_fips, pth) {
 
   df <- tigris::tracts(cb = T) %>% 
-    filter(STATEFP %in% stfp) %>% 
+    filter(STATEFP %in% state_fips) %>% 
     select(COUNTYFP, STATEFP, TRACTCE, NAME, GEOID, geometry) %>% 
     rename(cntyfp = COUNTYFP,
            stfp = STATEFP,
@@ -91,15 +90,15 @@ getTracts <- function(stfp) {
   df <- cbind(df, getBbox(df))
   df <- getCentroid(df)
 
-  exportJSON <- toJSON(tract_geo)
-  write(exportJSON, "../data/processed/tractGeoJSON.json")
+  exportJSON <- toJSON(df)
+  write(exportJSON, file.path(pth, "tractGeoJSON.json"))
 }
 
 #' Get Voting Districts
 #' Writes out a json file at the voting district level
-getVd <- function(stfp, year = 2020) {
+getVd <- function(state_fips, pth, year = 2020) {
   
-  vd_geo <- lapply(stfp, function(st) {
+  vd_geo <- lapply(state_fips, function(st) {
     
     df <- tigris::voting_districts(year = year, state=st, cb=T) %>% 
       rename(stfp = STATEFP20,
@@ -117,5 +116,35 @@ getVd <- function(stfp, year = 2020) {
   }) %>% dplyr::bind_rows()
   
   exportJSON <- toJSON(vd_geo)
-  write(exportJSON, "../data/processed/votingDistrictGeoJSON.json")
+  write(exportJSON, file.path(pth, "votingDistrictGeoJSON.json"))
+}
+
+#' Process longitudinal data
+getLongitudinal <- function(df, state_fips, years) {
+
+  df <- df %>% 
+    mutate(stfp = stringr::str_sub(fips_code, 1, 2),
+           cntyfp = stringr::str_sub(fips_code, 1, 5)) %>% 
+    filter(stfp %in% state_fips) %>%
+    filter(year %in% years) %>% 
+    select(fips_code, percentage_race_black_african_american, year, stfp, cntyfp) %>% 
+    rename(geoid = fips_code,
+           pctBlack = percentage_race_black_african_american,
+           baseYear = year)
+
+  return(df)
+}
+
+getCountiesLongitudinal <- function(df, state_fips, years, pth) {
+
+  df <- getLongitudinal(df, state_fips, years)
+  exportJSON <- toJSON(df)
+  write(exportJSON, file.path(pth, "countyLongitudinal.json"))
+}
+
+getTractsLongitudinal <- function(df, state_fips, years, pth) {
+
+  df <- getLongitudinal(df, state_fips, years)
+  exportJSON <- toJSON(df)
+  write(exportJSON, file.path(pth, "tractsLongitudinal.json"))
 }
--- src/services/main.R
+++ src/services/main.R
@@ -1,10 +1,15 @@
 source('./R/nest_data.R')
 
+pth <- "../data/processed"
+
 # State fips codes, adjust this list and rerun to add additional states
-stfp <- c("13", "45", "28", "55")
+state_fips <- c("13", "45", "28", "55")
+years <- c(2016, 2018, 2020, 2022)
 
 # Write out all the geo data
-getStates(stfp)
-getCounties(stfp)
-getTracts(stfp)
-getVd(stfp)
+getStates(state_fips, pth)
+getCounties(state_fips, pth)
+getTracts(state_fips, pth)
+getVd(state_fips, pth)
+getCountiesLongitudinal(readr::read_csv("../data/raw/county_year_summary_RI_World_01032024_fips.csv"), state_fips, years, pth)
+getTractsLongitudinal(readr::read_csv("../data/raw/census_tract_year_summary_RI_World_01032024_fips.csv"), state_fips, years, pth)
