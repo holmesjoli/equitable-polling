c5f677dc111fac27976631a1391fb4c836feaeef
Joli Holmes <holmesjoli@gmail.com>
2024-02-05 15:12:55 -0800

    commented out other pages

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -11,9 +11,9 @@ export function Navigation(): JSX.Element {
                 <h1><NavLink to="/">Equitable Polling Locations</NavLink></h1>
             </div>
             <div className="Navigation_links">
-                <NavLink to="/Trends" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><h3>Trends</h3></NavLink>
+                {/* <NavLink to="/Trends" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><h3>Trends</h3></NavLink>
                 <NavLink to="/Methodology" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><h3>Methodology</h3></NavLink>
-                <NavLink to="/About" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><h3>About</h3></NavLink>
+                <NavLink to="/About" className={({ isActive }) => (isActive ? 'active' : 'inactive')}><h3>About</h3></NavLink> */}
             </div>
         </div>
     )
