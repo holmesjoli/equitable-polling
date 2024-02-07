c1e587f70b190c1a3d58e65527a70bfa5cd9eb1f
Joli Holmes <holmesjoli@gmail.com>
2024-02-07 14:20:14 -0800

    added polling location data availability

--- src/pages/Methodology.tsx
+++ src/pages/Methodology.tsx
@@ -16,14 +16,79 @@ export default function Methodology({}): JSX.Element {
                             </ul>
                         </div>
                         <div className="Inner-h4">
-                            <h4>Demographic data</h4>
+                            <h4>Demographic Data</h4>
                             <ul>
                                 <li><a className="SemiBold" href="https://www.census.gov/programs-surveys/acs/data.html">American Community Survey (ACS)</a> files maintained by the US Census Bureau</li>
                             </ul>
                         </div>
-                        {/* <div className="Inner-h4">
+                        <div className="Inner-h4">
                             <h4>Polling Location Data</h4>
-                        </div> */}
+                            <ul>
+                                <li><a className="SemiBold" href="https://publicintegrity.org/">The Center for Public Integrity (CPI)</a></li>
+                                <li><a className="SemiBold" href="https://www.democracy.works/">Democracy Works (DW)</a></li>
+                            </ul>
+                        </div>
+                </div>
+                <div className="Inner-h3">
+                    <h3>Data Availablity</h3>
+                    <div className="Inner-h4">
+                        <h4>Geographic Boundaries</h4>
+                        <p>Although county boundaries stay fairly static year-over-year, the Census Bureau updates census tract geographies when the 
+                            decennial census is updated. For this reason, years 2012 – 2018 rely on census tract geographies from 2010, and years 2020 
+                            and 2022 rely on census tract geographies from 2020.</p>
+                    </div>
+                    <div className="Inner-h4">
+                        <h4>Polling Location Data</h4>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td>2012</td>
+                                    <td>2014</td>
+                                    <td>2016</td>
+                                    <td>2018</td>
+                                    <td>2020</td>
+                                    <td>2022</td>
+                                </tr>
+                                <tr>
+                                    <td>Georgia</td>
+                                    <td className="NA">NA</td>
+                                    <td className="NA">NA</td>
+                                    <td>CPI</td>
+                                    <td>CPI</td>
+                                    <td>CPI</td>
+                                    <td>DW</td>
+                                </tr>
+                                <tr>
+                                    <td>Mississippi</td>
+                                    <td>CPI</td>
+                                    <td>CPI</td>
+                                    <td>CPI</td>
+                                    <td>CPI</td>
+                                    <td className="NA">NA</td>
+                                    <td>DW</td>
+                                </tr>
+                                <tr>
+                                    <td>South Carolina</td>
+                                    <td>CPI</td>
+                                    <td>CPI</td>
+                                    <td>CPI</td>
+                                    <td>CPI</td>
+                                    <td>CPI</td>
+                                    <td>DW</td>
+                                </tr>
+                                <tr>
+                                <td>Wisconsin</td>
+                                <td>CPI</td>
+                                <td>CPI</td>
+                                <td>CPI</td>
+                                <td>CPI</td>
+                                <td>CPI</td>
+                                <td>DW</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
                 <div className="Inner-h3">
                     <h3>Data Caveats</h3>
@@ -35,13 +100,7 @@ export default function Methodology({}): JSX.Element {
                         </ul>
                     </div>
                     <div className="Inner-h4">
-                        <h4>Geographical Changes</h4>
-                        <p>Although county boundaries stay fairly static year-over-year, the Census Bureau updates census tract geographies when the 
-                            decennial census is updated. For this reason, years 2012 – 2018 rely on census tract geographies from 2010, and years 2020 
-                            and 2022 rely on census tract geographies from 2020.</p>
-                    </div>
-                    <div className="Inner-h4">
-                        <h4>Polling Location Data</h4>
+                        <h4>Polling Location Matching</h4>
                         <p>Polling locations are challenging to track because there is no systematic authority, government or otherwise, that tracks 
                             the locations year-over-year. Individual counties report polling locations to data aggregators like L2, however, there may 
                             be inconsistencies in the names of the locations and the exact latitudes and longitudes in the raw data.</p>
